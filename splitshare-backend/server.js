886f1496bfe07850094a0659c7b227ead8163c63
UniGami <sreenevedhunity@gmail.com>
2025-09-03 23:06:24 +0530

    Environment Setup

---
+++ splitshare-backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB Atlas connected'))
+.catch(err => console.log('MongoDB connection error:', err));
+app.get('/', (req, res) => {
+  res.send('Backend+Mongoose is running!');
+});
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
